cc5de7472c16eb69d929934ed5914e0f50261107
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2026-09-07 09:25:37 +0900

    samples: sysreg: Add power cycle command
    Add a` pwrcycle` shell command that requests TRCH to perform a power cycle
    via the FPGA. Sleep for `CONSOLE_MESSAGE_DELAY` before issuing the request
    to allow the status message to appear on the console. Print an error to
    the console if the power cycle does not complete within
    `POWER_CYCLE_TIMEOUT`.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- samples/sysreg/src/main.c
+++ samples/sysreg/src/main.c
@@ -4,9 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <zephyr/shell/shell.h>
 
 #include "sc_sysreg.h"
+
+#define POWER_CYCLE_TIMEOUT   K_SECONDS(1)
+#define CONSOLE_MESSAGE_DELAY K_MSEC(100)
 
 static int cmd_cfgmem_show(const struct shell *sh, size_t argc, char **argv)
 {
@@ -44,6 +48,29 @@ static int cmd_cfgmem_select(const struct shell *sh, size_t argc, char **argv)
 	return 0;
 }
 
+static int cmd_power_cycle(const struct shell *sh, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	shell_print(sh, "Requesting TRCH to perform a power cycle via FPGA");
+
+	/* Sleep to allow the message to appear on the console. */
+	k_sleep(CONSOLE_MESSAGE_DELAY);
+
+	sc_power_cycle();
+
+	/*
+	 * Print an error to the console if the power cycle does not complete
+	 * within POWER_CYCLE_TIMEOUT.
+	 */
+	k_sleep(POWER_CYCLE_TIMEOUT);
+
+	shell_error(sh, "Power cycle request timed out");
+
+	return -ETIMEDOUT;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(cfgmem_cmds,
 			       SHELL_CMD_ARG(select, NULL,
 					     "Select Configuration Memory bank 0 or 1",
@@ -54,6 +81,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(cfgmem_cmds,
 			       SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(cfgmem, &cfgmem_cmds, "Configuration Memory commands", NULL);
+SHELL_CMD_REGISTER(pwrcycle, NULL, "Request TRCH to perform a power cycle via FPGA",
+		   cmd_power_cycle);
 
 int main(void)
 {
