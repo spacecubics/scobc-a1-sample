d57078b8e24789c3a37a9341ea3bcf7d318ac973
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2026-09-07 09:25:37 +0900

    samples: sysreg: Add Configuration bank selection command
    Add a `cfgmem select` command to select the configuration memory
    bank. Do not check whether the selected bank differs from the current
    bank. After switching the bank, read the register again and display
    the currently selected bank.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- samples/sysreg/src/main.c
+++ samples/sysreg/src/main.c
@@ -19,7 +19,35 @@ static int cmd_cfgmem_show(const struct shell *sh, size_t argc, char **argv)
 	return 0;
 }
 
+static int cmd_cfgmem_select(const struct shell *sh, size_t argc, char **argv)
+{
+	int err = 0;
+	int ret;
+	unsigned long bank;
+
+	ARG_UNUSED(argc);
+
+	bank = shell_strtoul(argv[1], 10, &err);
+	if (err != 0 || bank > SC_CFG_MEM_1) {
+		shell_error(sh, "Bank must be 0 or 1");
+		return -EINVAL;
+	}
+
+	ret = sc_select_cfgmem((enum sc_cfgmem)bank);
+	if (ret < 0) {
+		shell_error(sh, "Failed to select Configuration Memory bank %lu: %d", bank, ret);
+		return ret;
+	}
+
+	shell_print(sh, "Current bank : %d", sc_get_cfgmem());
+
+	return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(cfgmem_cmds,
+			       SHELL_CMD_ARG(select, NULL,
+					     "Select Configuration Memory bank 0 or 1",
+					     cmd_cfgmem_select, 2, 0),
 			       SHELL_CMD(show, NULL,
 					 "Show the boot and current Configuration Memory banks",
 					 cmd_cfgmem_show),
